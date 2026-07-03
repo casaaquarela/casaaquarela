6b79d2c86efe4dfd9caaf8c9088fdd8fa7dbd7f8
casaaquarela <casaaquarela.jundiai@gmail.com>
2026-07-03 16:34:11 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,4 @@
-const CACHE = 'casaaquarela-v1';
-const ASSETS = ['/', '/index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+// Service Worker simplificado - sem cache que interfere
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+// Não intercepta nenhum fetch - deixa tudo passar normalmente
